c7d752155610e75502e795a4b30653fda600b704
28davidrusnac-dotcom <28.david.rusnac@carolinaday.org>
2026-01-22 11:02:25 -0500

    Update hi3.py

--- hi3.py
+++ hi3.py
@@ -1,1 +1,14 @@
+# We will have 3 doors: 1 door is the winner
+# The contestant picks one door, then the host opens a different door
+#The contestant should change their potion
+#We reveal if the contestant has the right door
 
+import random
+
+def monty_hall():
+  doors = [0,1,2]
+
+winning_Door = random.randint(1,3)
+contestChoice = random.randint(1,3)
+
+print("winner: + str(winningDoor) + "; Contestant Choice:"+ str(contestChoice)
