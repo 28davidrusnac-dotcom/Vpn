cfb8ce61603c0036ece72971f7b51658ba965ccb
28davidrusnac-dotcom <28.david.rusnac@carolinaday.org>
2026-01-29 19:49:22 +0000

    Hi

---
+++ hi6.py
@@ -0,0 +1,3 @@
+myNumberList = [10, 3, 5, 11, 15, 11, 13, 3, 7, 11, 1, 5, 7, 14, 4, 9, 10, 11, 11, 8, 12, 1, 12, 1, 2]
+for i in range (len(myNumberList)):
+    myNumberList.append(myNumberList[i]+1)
